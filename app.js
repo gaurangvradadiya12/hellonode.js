1a0bb853ef87cf56abd4b6fc12372b472a54b842
gaurangvradadiya12 <gaurangvradadiya12@gmail.com>
2025-07-11 08:52:59 +0530

    add branch

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log("This is a simple Node.js application.\n");
 console.log("It prints a greeting message to the console.\n");  
 console.log("Hello World!\n");
+console.log("This application is running on OpenShift.\n");
+console.log("You can access it via the provided URL.\n");   
