5a33f1fb4ae426de6a7e7293f01ed09857540d45
gaurangvradadiya12 <gaurangvradadiya12@gmail.com>
2025-07-11 08:48:24 +0530

    seq change

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-console.log("Hello World!\n");
 console.log("This is a simple Node.js application.\n");
 console.log("It prints a greeting message to the console.\n");  
+console.log("Hello World!\n");
